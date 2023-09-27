102329d461de6b8c9f9f47be5bf803057fb768bf
muskanthapa2000 <7022muskan@gmail.com>
2023-09-28 02:12:59 +0530

    d

--- apple_frontend/src/components/Footer.jsx
+++ apple_frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Text, Link, Stack, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Flex, Text, Link, Stack, IconButton, Center } from '@chakra-ui/react';
 import {
   FaFacebook,
   FaLinkedin,
@@ -12,36 +12,44 @@ import {
 } from 'react-icons/fa';
 
 const Footer = () => {
-  const columnDirection = useBreakpointValue({ base: 'column', md: 'row' });
-
   return (
     <Box bg="gray" py={8} color="white">
+     
+
+      
       <Flex
-        direction={columnDirection}
+        direction={{ base: 'column', md: 'row' }} // Responsive column layout
         maxW="1200px"
         mx="auto"
         justify="space-between"
         px={4}
-        flexWrap="wrap"
+        align="Center"
+        
+        flexWrap="wrap" // Wrap content to the next line on small screens
       >
+        {/* Footer Column 1 */}
         <Stack direction="row" spacing={4} mb={{ base: 4, md: 0 }}>
-          {/* Footer Column 1 */}
           <Flex alignItems="center">
             <IconButton
               icon={<FaPhone />}
               aria-label="Phone Icon"
-              fontSize="24px"
-              mr={2}
+              fontSize="12px"
+              ml={-8}
               variant="unstyled"
             />
+      
+              
+           
             <Stack spacing={1}>
               <Text>Working</Text>
               <Text>24*7</Text>
               <Text>from 8.00 - 9.00</Text>
             </Stack>
           </Flex>
+        </Stack>
 
-          {/* Footer Column 2 */}
+        {/* Footer Column 2 */}
+        <Stack direction="row" spacing={4} mb={{ base: 4, md: 0 }}>
           <Flex alignItems="center">
             <Stack spacing={1}>
               <Text>Free Delivery</Text>
@@ -49,8 +57,10 @@ const Footer = () => {
               <Text>from $2.500</Text>
             </Stack>
           </Flex>
+        </Stack>
 
-          {/* Footer Column 3 */}
+        {/* Footer Column 3 */}
+        <Stack direction="row" spacing={4}>
           <Flex alignItems="center">
             <Stack spacing={1}>
               <Text>Support</Text>
@@ -60,14 +70,15 @@ const Footer = () => {
           </Flex>
         </Stack>
 
-        <Stack direction="column" spacing={4}>
-          {/* Footer Column 4 */}
+        {/* Footer Column 4 */}
+        <Stack direction="column" spacing={4} mt={{ base: 4, md: 0 }}>
           <Text fontSize="xl">0332 253 3535</Text>
           <Text>Online Store helpline</Text>
         </Stack>
       </Flex>
-
+     
       {/* Footer Row 2 */}
+      
       <Flex
         maxW="1200px"
         mx="auto"
@@ -76,8 +87,13 @@ const Footer = () => {
         py={4}
         px={4}
         mt={4}
+      
+        flexWrap="wrap" // Wrap content to the next line on small screens
       >
-        <Text>&copy; 2023 AAMM All Right Reserved</Text>
+        <Text fontSize={{ base: 'sm', md: 'md' }}>
+          &copy; 2023 AAMM All Right Reserved
+        </Text>
+
         <Flex>
           <Link href="#" isExternal>
             <IconButton
@@ -115,6 +131,7 @@ const Footer = () => {
             />
           </Link>
         </Flex>
+
         <Flex>
           <Text fontSize="xl" mr={2}>
             Accepting payment methods
